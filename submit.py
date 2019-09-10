81baf8104b3b46d9bf8cfd2b1d9baceb1220cb77
madhanbose99 <38097174+madhanbose99@users.noreply.github.com>
2019-09-10 16:40:28 +0530

    Initial Commit

--- submit.py
+++ submit.py
@@ -3,119 +3,58 @@ import numpy as np
 sys.path.append("..")
 import grading
 
-from mdp import MDP, FrozenLakeEnv
+
+def submit_experience_replay(rewards_replay, rewards_baseline, email, token):
+    flag1 = np.mean(rewards_replay[:100]) - np.mean(rewards_baseline[:100])
+    flag2 = np.mean(rewards_replay[-100:])
+    flag3 = np.mean(rewards_baseline[-100:])
+
+    grader = grading.Grader("XUt-8d7yEee8nwq8KJgXXg")
+    grader.set_answer("iEQwT", flag1)
+    grader.set_answer("8N1Wm", flag2)
+    grader.set_answer("F0Am8", flag3)
+
+    grader.submit(email, token)
+
+    
+def submit_qlearning1(rewards, email, token):
+    flag1 = np.mean(rewards[-10:])
+
+    grader = grading.Grader("XbjcGd7xEeeDzRKutDCmyA")
+    grader.set_answer("5NB4z", flag1)
+
+    grader.submit(email, token)
 
 
-def submit_assigment(
-        get_action_value,
-        get_new_state_value,
-        get_optimal_action,
-        value_iteration,
-        email,
-        token):
-    grader = grading.Grader("EheZDOgLEeenIA4g5qPHFA")
-    sys.stdout = None
+def submit_qlearning2(rewards, email, token):
+    flag1 = np.mean(rewards[-10:])
 
-    transition_probs = {
-        's0': {
-            'a0': {'s1': 0.8, 's2': 0.2},
-            'a1': {'s1': 0.2, 's2': 0.8},
-        },
-        's1': {
-            'a0': {'s0': 0.2, 's2': 0.8},
-            'a1': {'s0': 0.8, 's2': 0.2},
-        },
-        's2': {
-            'a0': {'s3': 0.5, 's4': 0.5},
-            'a1': {'s3': 1.0},
-        },
-        's3': {
-            'a0': {'s1': 0.9, 's2': 0.1},
-            'a1': {'s1': 0.7, 's2': 0.3},
-        },
-        's4': {
-            'a0': {'s3': 1.0},
-            'a1': {'s3': 0.7, 's1': 0.3},
-        }
-    }
-    rewards = {
-        's0': {'a0': {'s1': 0, 's2': 1}, 'a1': {'s1': 0, 's2': 1}},
-        's1': {'a0': {'s0': -1, 's2': 1}, 'a1': {'s0': -1, 's2': 1}},
-        's2': {'a0': {'s3': 0, 's4': 1}, 'a1': {'s3': 0, 's4': 1}},
-        's3': {'a0': {'s1': -3, 's2': -3}, 'a1': {'s1': -3, 's2': -3}},
-        's4': {'a1': {'s1': +10}}
-    }
+    grader = grading.Grader("XbjcGd7xEeeDzRKutDCmyA")
+    grader.set_answer("CkyJ4", flag1)
 
-    mdp = MDP(transition_probs, rewards, initial_state='s0')
+    grader.submit(email, token)
 
-    test_Vs = {s: i for i, s in enumerate(mdp.get_all_states())}
-    qvalue1 = get_action_value(mdp, test_Vs, 's1', 'a0', 0.9)
-    qvalue2 = get_action_value(mdp, test_Vs, 's4', 'a1', 0.9)
 
-    grader.set_answer("F16dC", qvalue1 + qvalue2)
+def submit_qlearning_all(rewards_q1, rewards_q2, email, token):
+    grader = grading.Grader("XbjcGd7xEeeDzRKutDCmyA")
+    
+    flag1 = np.mean(rewards_q1[-10:])
+    grader.set_answer("5NB4z", flag1)
 
-    # ---
+    flag2 = np.mean(rewards_q2[-10:])
+    grader.set_answer("CkyJ4", flag2)
 
-    svalue1 = get_new_state_value(mdp, test_Vs, 's2', 0.9)
-    svalue2 = get_new_state_value(mdp, test_Vs, 's4', 0.9)
+    grader.submit(email, token)
 
-    grader.set_answer("72cBp", svalue1 + svalue2)
 
-    # ---
+def submit_sarsa(rewards_ql, rewards_sarsa, email, token):
+    flag1 = np.mean(rewards_ql[-100:])
+    flag2 = np.mean(rewards_sarsa[-100:])
+    flag3 = np.mean(rewards_sarsa[-100:]) - np.mean(rewards_ql[-100:])
 
-    state_values = {s: 0 for s in mdp.get_all_states()}
-    gamma = 0.9
+    grader = grading.Grader("pazQX97xEee_JA6t1Myltg")
+    grader.set_answer("ZarWJ", flag1)
+    grader.set_answer("izJi4", flag2)
+    grader.set_answer("frgbU", flag3)
 
-    # ---
-
-    action1 = get_optimal_action(mdp, state_values, 's1', gamma)
-    action2 = get_optimal_action(mdp, state_values, 's2', gamma)
-
-    grader.set_answer("xIuti", action1 + action2)
-
-    # ---
-
-    s = mdp.reset()
-    rewards = []
-    for _ in range(10000):
-        s, r, done, _ = mdp.step(get_optimal_action(mdp, state_values, s, gamma))
-        rewards.append(r)
-
-    grader.set_answer("Y8g0j", np.mean(rewards) + np.std(rewards))
-
-    mdp = FrozenLakeEnv(slip_chance=0.25)
-    state_values = value_iteration(mdp)
-    gamma = 0.9
-
-    total_rewards = []
-    for game_i in range(1000):
-        s = mdp.reset()
-        rewards = []
-        for t in range(100):
-            s, r, done, _ = mdp.step(get_optimal_action(mdp, state_values, s, gamma))
-            rewards.append(r)
-            if done: break
-        total_rewards.append(np.sum(rewards))
-
-    grader.set_answer("ABf1b", np.mean(total_rewards) + np.std(total_rewards))
-
-    # ---
-
-    mdp = FrozenLakeEnv(slip_chance=0.25, map_name='8x8')
-    state_values = value_iteration(mdp)
-    gamma = 0.9
-
-    total_rewards = []
-    for game_i in range(1000):
-        s = mdp.reset()
-        rewards = []
-        for t in range(100):
-            s, r, done, _ = mdp.step(get_optimal_action(mdp, state_values, s, gamma))
-            rewards.append(r)
-            if done: break
-        total_rewards.append(np.sum(rewards))
-
-    grader.set_answer("U3RzE", np.mean(total_rewards) + np.std(total_rewards))
-
-    sys.stdout = sys.__stdout__
     grader.submit(email, token)
